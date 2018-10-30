81816fdd0740fad87fc6557684371bdbac4f976c
Adrian Melo <31514444+adrianmb0@users.noreply.github.com>
2018-10-29 22:56:48 -0500

    Update song.cpp

--- song.cpp
+++ song.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <cstring>
 #include "song.h"
 
 using namespace std;
@@ -41,4 +42,54 @@ bool Song::operator>(Song const &rhs){                  //check this before movi
         return(false);
     }
     
+}
+
+bool Song::operator<(Song const &rhs){                  //check this before moving on
+    for(int i=0; artist[i]!= "\0"; i++){
+        if(artist[i]=="\0"){
+            return(true);                               //edge case i.e. "AB" vs "ABC"
+        }else if(artist[i]<rhs.artist[i]){
+            return(false);
+        }else{
+            return(true);
+        }
+    }
+    
+    for(int i=0; title[i]!= "\0"; i++){
+        if(title[i]<rhs.title[i]){
+            return(false);
+        }else{
+            return(true);
+        }
+    }
+    
+    if(size>rhs.size){
+        return(true);
+    }else{
+        return(false);
+    }
+}
+
+bool Song::operator==(Song const &rhs){
+    if
+    for(int i=0; artist[i]!= "\0"; i++){
+        if(artist[i]<rhs.artist[i]){
+            return(false);
+        }
+    }
+    if 
+    
+    for(int i=0; title[i]!= "\0"; i++){
+        if(title[i]<rhs.title[i]){
+            return(false);
+        }else{
+            return(true);
+        }
+    }
+    
+    if(size>rhs.size){
+        return(true);
+    }else{
+        return(false);
+    }
 }
